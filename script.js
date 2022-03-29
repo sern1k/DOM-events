392e523a8e8db273b57a4f5c2908ec31b5179c01
Pola Madej <66171277+sern1k@users.noreply.github.com>
2022-03-29 10:43:54 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+
+function inputLenght() {
+	return input.value.length;
+}
+
+function createListElement() {
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+	ul.appendChild(li);
+	input.value = "";
+}
+
+function addListAfterClick () {
+	if (inputLenght() > 0) {
+		createListElement();
+	}
+}
+
+function AddListAfterKeypress (event) {
+	if (inputLenght() > 0 && event.keyCode === 13) {
+		createListElement();
+	}
+}
+
+button.addEventListener("click", addListAfterClick)
+input.addEventListener("keypress", AddListAfterKeypress)
